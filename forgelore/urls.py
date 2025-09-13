0f437390d737a0eebe8e552e4e310ffe63af4f98
maestromaximo <alejandrogarcia2423@hotmail.com>
2025-09-13 11:49:02 -0400

    Refactor URL routing to modularize main app URLs
    - Updated forgelore/urls.py to include main app URLs, improving organization.
    - Created main/urls.py to define routes for home, dashboard, login, logout, and signup views.

--- forgelore/urls.py
+++ forgelore/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from main import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('', include('main.urls')),
 ]
